5b160159dd9de5b0ebefa9c0f908edb4124d761c
Tama <tamawish@gmail.com>
2020-04-17 22:03:55 +0900

    Peopleテーブル作成

---
+++ src/Controller/PeopleController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class PeopleController extends AppController {
+
+    public function index() {
+        $data = $this->People->find('all')->toArray();
+        $this->set('data', $data);
+    }
+}
