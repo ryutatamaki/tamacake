d33e14dea80370a867d7a3f7c6e0515a2cc3e8e7
Tama <tamawish@gmail.com>
2020-04-17 11:47:43 +0900

    hellocontroller,view作成

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class HelloController extends AppController {
+
+    public function index() {
+        $this->viewBuilder()->autoLayout(false);
+        $this->set('title', 'Hello!');
+        $this->set('message', 'This is message!');
+    }
+}
