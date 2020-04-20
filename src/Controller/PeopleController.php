52c83828afca8d3de6d3a7d1cd73812e6c10adfa
Tama <tamawish@gmail.com>
2020-04-20 17:22:10 +0900

    add create作成

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -6,7 +6,22 @@ use App\Controller\AppController;
 class PeopleController extends AppController {
 
     public function index() {
-        $data = $this->People->find('all')->toArray();
+        $data = $this->People->find('all');
         $this->set('data', $data);
+    }
+
+    public function add() {
+        $entity = $this->People->newEntity();
+        $this->set('entity', $entity);
+    }
+
+    public function create() {
+        if ($this->request->is('post')) {
+            $data = $this->request->data['People'];
+            $entity = $this->People->newEntity($data);
+            $this->People->save($entity);
+        }
+
+        return $this->redirect(['action' => 'index']);
     }
 }
